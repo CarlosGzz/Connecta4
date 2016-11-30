fac9bde7794b89416ab17785972790c5f58bd005
CarlosGzz <carlosgzz93@gmail.com>
2016-11-30 14:36:01 -0600

    model

--- Modelo/connect.php
+++ Modelo/connect.php
@@ -1,13 +1,13 @@
 <?php
-	/*$servername = "us-cdbr-iron-east-04.cleardb.net";
+	$servername = "us-cdbr-iron-east-04.cleardb.net";
 	$username = "b1ac66f5ee860f";
 	$password = "d32eb0ea";
-	$dbname = "heroku_1f1d4a73d7ba315";*/
+	$dbname = "heroku_1f1d4a73d7ba315";
 
-	$servername = "localhost";
+	/*$servername = "localhost";
 	$username = "root";
 	$password = "kobyjzt";
-	$dbname = "conecta4";
+	$dbname = "conecta4";*/
 
 	// Create connection
 	$db = new mysqli($servername, $username, $password, $dbname);
